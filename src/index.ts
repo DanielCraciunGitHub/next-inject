e8c0225939c9bcc905687e77398649bccff816e9
Daniel Craciun <danielcraciun111@yahoo.com>
2024-08-27 13:05:18 +0100

    feat: force latest CLI usage

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,9 @@ import { auth } from "./commands/auth"
 import { init } from "./commands/init"
 import { bootstrap } from "./commands/bootstrap"
 import { rename } from "./commands/rename"
+import latestVersion from "latest-version"
+import { logger } from "./utils/logger"
+import { handleError } from "./utils/handle-error"
 
 process.on("SIGINT", () => process.exit(0))
 process.on("SIGTERM", () => process.exit(0))
@@ -19,7 +22,7 @@ async function main() {
   const program = new Command()
     .name("next-inject")
     .description("Inject plugins into your next.js project")
-    .version("1.2.7", "-v, --version", "display the version number")
+    .version("1.2.8", "-v, --version", "display the version number")
 
   program
     .addCommand(add)
@@ -27,15 +30,15 @@ async function main() {
     .addCommand(init)
     .addCommand(bootstrap)
     .addCommand(rename)
-    // .hook("preSubcommand", async function (thisCommand: Command) {
-    //   const thisVersion = thisCommand.version()
-    //   const realVersion = await latestVersion('next-inject')
-    //   if (thisVersion !== realVersion) {
-        
-    //   }
-    //   console.log(`This version: ${thisVersion}`)
-    //   console.log(`Real version: ${realVersion}`)
-    // })
+    .hook("preSubcommand", async function (thisCommand: Command) {
+      const thisVersion = thisCommand.version()
+      const realVersion = await latestVersion("next-inject")
+
+      if (thisVersion !== realVersion) {
+        logger.warn("Please upgrade your CLI to the latest version.")
+        handleError("Try `npm update -g next-inject`")
+      }
+    })
 
   program.parse()
 }
